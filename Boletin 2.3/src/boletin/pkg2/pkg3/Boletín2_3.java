14ddefddaca91dfce148ec797a37c806131347b9
antonio <antonio@biruseto>
2016-10-10 08:57:44 +0200

    boletin 2.3 variante terminado

---
+++ Boletin 2.3/src/boletin/pkg2/pkg3/Boletín2_3.java
@@ -0,0 +1,14 @@
+package boletin.pkg2.pkg3;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletín2_3 {
+
+    public static void main(String[] args) {
+        float euros; //declaro euros
+        euros = Float.parseFloat(JOptionPane.showInputDialog("introduzca la cantidad de euros "));
+        JOptionPane.showMessageDialog(null," La cantidad de cambio a dolares es de "+  euros*0.89);
+    }
+    
+}
